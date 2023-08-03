16f6149051e86a24f280f8f983b5f45960b9863d
afshan ahmed khan <afshanahmeda2k@gmail.com>
2023-08-04 01:46:42 +0530

    fixing cors issue

--- server/app.js
+++ server/app.js
@@ -21,6 +21,13 @@ mongoose.connection.on("error",()=>{
    console.log("error connecting to mongo " ,)
 })
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(cors())
 // app.use(express.json())
 app.use(router)
